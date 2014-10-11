982cb51915fd7c4714af120cefb905912d4590f5
good72 <good72@users.noreply.github.com>
2014-10-11 14:45:26 +0100

    finished

--- plot3.R
+++ plot3.R
@@ -16,7 +16,7 @@ d1 <- (filter(d, d$Date == '1/2/2007' | d$Date == '2/2/2007'));
 
 png(filename="plot3.png", width=480, height=480, bg="white");
 
-plot(d3$aat, d3$Sub_metering_1, type="l", ylab="Energy sub metering")
+plot(d3$aat, d3$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
 lines(d3$aat, d3$Sub_metering_2, type="l", col="red")
 lines(d3$aat, d3$Sub_metering_3, type="l", col="blue")
 legend("topright", col=c("black","red","blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=1)
